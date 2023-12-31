11639b7f11479408194b06f639da51ba3f485931
Mr.Link <108998528+MrKarimov01@users.noreply.github.com>
2023-12-31 17:46:54 +0500

    secent code

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import { init } from 'console-ban'
+
+
+
+// init({
+//   // Rewrite the <body></body> as a string
+//   write: '<h1> Stop </h1>',
+//   redirect: "Salom"
+
+// })
+const App = () => {
+
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
